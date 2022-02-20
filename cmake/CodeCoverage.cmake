d92c3ad7659b59924b1bd04936131d8e59bfb57b
s-merritt <smerritt987@gmail.com>
2022-02-20 12:29:27 -0600

    update CodeCoverage command to ignore exception branches generated by the compiler

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -151,7 +151,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE_COBERTURA _targetname _testrunner _outputname
 		${_testrunner} ${ARGV3}
 
 		# Running gcovr
-		COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/test/' -e '${CMAKE_SOURCE_DIR}/build/' -e '${CMAKE_SOURCE_DIR}/lib/'  -o ${_outputname}.xml
+		COMMAND ${GCOVR_PATH} --xml-pretty --exclude-throw-branches -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/test/' -e '${CMAKE_SOURCE_DIR}/build/' -e '${CMAKE_SOURCE_DIR}/lib/'  -o ${_outputname}.xml
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 		COMMENT "Running gcovr to produce Cobertura code coverage report."
 	)
